75cdae1e24fd94efc6a47f52839856ad8c8c8c48
davilanco <davilanco37@gmail.com>
2026-09-21 23:32:38 +0100

    Create login.js

---
+++ functions/api/login.js
@@ -0,0 +1,6 @@
+export async function onRequestPost({request, env}) {
+  const {login,password} = await request.json();
+  const user = await env.DB.prepare("SELECT * FROM users WHERE nickname=? OR email=? OR phone=?").bind(login,login,login).first();
+  if(!user || user.password!==password) return Response.json({error:"Invalid"}, {status:401});
+  return Response.json({ok:true,user});
+    }
