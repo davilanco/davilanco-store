4dfda161b64e3290c79267427cbbe9531bfa243e
davilanco <davilanco37@gmail.com>
2026-09-22 05:51:33 +0100

    Update login.js

--- functions/api/login.js
+++ functions/api/login.js
@@ -1,6 +1,7 @@
-export async function onRequestPost({request, env}) {
+export async function onRequestPost(context) {
+  const {request, env} = context;
   const {login,password} = await request.json();
   const user = await env.DB.prepare("SELECT * FROM users WHERE nickname=? OR email=? OR phone=?").bind(login,login,login).first();
-  if(!user || user.password!==password) return Response.json({error:"Invalid"}, {status:401});
+  if(!user || user.password!==password) return Response.json({error:"Invalid login"}, {status:401});
   return Response.json({ok:true,user});
-    }
+}
